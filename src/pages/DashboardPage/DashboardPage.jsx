859464bf27db1edf2bda48d86d56a99975b5c862
sevchik-f8fe <selddevelopment1111@gmail.com>
2025-10-26 13:53:44 +0300

    fix dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,252 +1,4 @@
-// import { Box, TextField, InputAdornment, Button, FormGroup, Checkbox, FormControlLabel, Typography } from "@mui/material";
-// import SearchIcon from '@mui/icons-material/Search';
-// import { nanoid } from "nanoid";
-// import ListItem from "../../components/ListItem";
-// import { useRef } from "react";
-// import { useDispatch, useSelector, useStore } from "react-redux";
-// import { setData, setNextPage, setPage, setQuery, setScrollField } from "./DashboardSlice";
-// import { debounce } from "lodash";
-// import axios from "axios";
-// import { Bounce, toast } from "react-toastify";
-// import { combineAndShuffleArrays } from "../../util/dashboard";
-// import { setSimpleField } from "../SignUpPage/AuthSlice";
-// import useInfiniteScroll from 'react-infinite-scroll-hook';
-
-// const DashboardPage = () => {
-//     const dispatch = useDispatch();
-//     const { page, query, isGif, hasMore, isVideo, tenorNext, isImg, data } = useSelector((state) => state.dashboard);
-//     const { loading } = useSelector(state => state.auth);
-//     const store = useStore();
-
-//     const notify = () => toast.error("Что-то пошло не так :(", {
-//         position: "bottom-left",
-//         autoClose: 3000,
-//         hideProgressBar: true,
-//         closeOnClick: false,
-//         draggable: true,
-//         progress: undefined,
-//         theme: "dark",
-//         transition: Bounce,
-//     });
-
-//     const queryRef = useRef(null);
-//     const isImgRef = useRef(null);
-//     const isVideoRef = useRef(null);
-//     const isGifRef = useRef(null);
-
-//     const clickHandle = debounce((data) => {
-//         dispatch(setQuery({ query: data.query, isImg: data.isImg, isVideo: data.isVideo, isGif: data.isGif }));
-//     }, 500);
-
-//     const getContentWithoutQuery = async () => {
-//         const tenor = isGif && await axios.post('http://127.0.0.1:3000/api/tenor/list', { next: tenorNext }, { headers: { 'Content-Type': 'application/json' } })
-//             .then((res) => res?.data?.tenor)
-//             .then(res => {
-//                 dispatch(setNextPage({ field: 'tenorNext', next: res.next }))
-//                 return res.results;
-//             })
-//             .catch((err) => {
-//                 console.log(err)
-//                 notify();
-//             });
-
-//         const photos = isImg && await axios.post('http://127.0.0.1:3000/api/photos/list', { page }, { headers: { 'Content-Type': 'application/json' } })
-//             .then((res) => res?.data?.photos?.photos)
-//             .catch((err) => {
-//                 console.log(err)
-//                 notify();
-//             });
-
-//         if (combineAndShuffleArrays(photos || [], tenor || []).length == 0) dispatch(setScrollField({ field: 'hasMore', value: false }))
-//         return combineAndShuffleArrays(photos || [], tenor || []);
-//     }
-
-//     const getContentByQuery = async () => {
-//         const tenor = isGif && await axios.post('http://127.0.0.1:3000/api/tenor/search', { page: page, query }, { headers: { 'Content-Type': 'application/json' } })
-//             .then((res) => res?.data?.tenor)
-//             .then(res => {
-//                 console.log('tenor ', res)
-//                 return res;
-//             })
-//             .catch((err) => {
-//                 console.log(err)
-//                 notify();
-//             });
-
-//         const photos = isImg && await axios.post('http://127.0.0.1:3000/api/photos/photosByQuery', { page, query }, { headers: { 'Content-Type': 'application/json' } })
-//             .then((res) => {
-//                 console.log('photos ', res?.data?.photos?.photos)
-//                 return res?.data?.photos?.photos
-//             })
-//             .catch((err) => {
-//                 console.log(err)
-//                 notify();
-//             });
-
-//         const storyblock_v = isVideo && await axios.post('http://127.0.0.1:3000/api/storyblock/videosByQuery', { page, query }, { headers: { 'Content-Type': 'application/json' } })
-//             .then((res) => {
-//                 console.log('storyblock_v ', res?.data?.storyblock.results)
-//                 return res?.data?.storyblock.results
-//             })
-//             .catch((err) => {
-//                 console.log(err)
-//                 notify();
-//             });
-
-//         console.log('acc: ', combineAndShuffleArrays(photos || [], tenor || [], storyblock_v || []))
-//         if (combineAndShuffleArrays(photos || [], tenor || [], storyblock_v || []).length == 0) dispatch(setScrollField({ field: 'hasMore', value: false }))
-//         return combineAndShuffleArrays(photos || [], tenor || [], storyblock_v || []);
-//     }
-
-//     const loadMore = async () => {
-//         dispatch(setSimpleField({ field: 'loading', value: true }))
-//         dispatch(setScrollField({ field: 'hasMore', value: true }))
-//         dispatch(setPage({ page: page + 1 }))
-
-//         if (query.length == 0) {
-//             await getContentWithoutQuery()
-//                 .then((res) => dispatch(setData({ data: [...store.getState().dashboard.data, ...res] })))
-//                 .catch(e => console.log(e))
-//                 .finally(() => dispatch(setSimpleField({ field: 'loading', value: false })))
-//         } else {
-//             console.log(query, 'wq page', page, tenorNext);
-//             console.log('wq data: ', data)
-//             await getContentByQuery()
-//                 .then((res) => dispatch(setData({ data: [...store.getState().dashboard.data, ...res] })))
-//                 .catch(e => console.log(e))
-//                 .finally(() => dispatch(setSimpleField({ field: 'loading', value: false })))
-//         }
-
-//     };
-
-//     const newFetch = async () => {
-//         dispatch(setSimpleField({ field: 'loading', value: true }))
-//         dispatch(setScrollField({ field: 'hasMore', value: true }))
-//         dispatch(setPage({ page: 1 }))
-//         dispatch(setNextPage({ field: 'tenorNext', value: null }));
-
-//         if (query.length == 0) {
-//             await getContentWithoutQuery()
-//                 .then((res) => dispatch(setData({ data: res })))
-//                 .catch(e => console.log(e))
-//                 .finally(() => dispatch(setSimpleField({ field: 'loading', value: false })))
-//         } else {
-//             console.log(query, 'wq page', page, tenorNext);
-//             console.log('wq data: ', data)
-//             await getContentByQuery()
-//                 .then((res) => dispatch(setData({ data: res })))
-//                 .catch(e => console.log(e))
-//                 .finally(() => dispatch(setSimpleField({ field: 'loading', value: false })))
-//         }
-//     }
-
-//     const [sentryRef] = useInfiniteScroll({
-//         loading,
-//         hasNextPage: hasMore,
-//         onLoadMore: loadMore,
-//     });
-
-//     return (
-//         <Box sx={{ backgroundColor: '#F2EBFB30', backdropFilter: 'blur(10px)', border: '1px solid #D4BBFC', borderRadius: '1em', p: '1em', maxWidth: '80%', minWidth: '80%', m: '4em auto 2em auto' }}>
-//             <Typography variant="h1" gutterBottom>поиск</Typography>
-//             <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '2em' }}>
-
-//                 <TextField
-//                     InputProps={{
-//                         style: {
-//                             color: '#F2EBFB',
-//                         },
-//                     }}
-//                     size="small"
-//                     inputRef={queryRef}
-//                     type={'text'}
-//                     variant="outlined"
-//                     placeholder={'используйте английский язык'}
-//                     color='primary'
-//                     sx={{ minWidth: '50%' }}
-//                     slotProps={{
-//                         input: {
-//                             startAdornment: (
-//                                 <InputAdornment position="start">
-//                                     <SearchIcon sx={{ color: '#F2EBFB' }} />
-//                                 </InputAdornment>
-//                             ),
-//                         },
-//                     }}
-//                 />
-//                 <Button
-//                     loading={loading}
-//                     onClick={() => {
-//                         clickHandle({
-//                             query: queryRef.current.value.replace(/[^a-zA-Z ]/g, ""),
-//                             isImg: isImgRef.current.checked,
-//                             isVideo: isVideoRef.current.checked,
-//                             isGif: isGifRef.current.checked,
-//                         })
-
-//                         newFetch()
-//                     }
-
-//                     }
-//                     variant="contained"
-//                 >
-//                     поиск
-//                 </Button>
-
-//                 <FormGroup sx={{ display: 'flex', flexDirection: 'row' }}>
-//                     <FormControlLabel
-//                         control={<Checkbox
-//                             slotProps={{
-//                                 input: {
-//                                     ref: isImgRef,
-//                                 },
-//                             }}
-//                             defaultChecked
-//                         />}
-//                         label="картинки"
-//                     />
-//                     <FormControlLabel
-//                         control={<Checkbox
-//                             slotProps={{
-//                                 input: {
-//                                     ref: isVideoRef,
-//                                 },
-//                             }}
-//                             defaultChecked
-//                         />}
-//                         label="видео"
-//                     />
-//                     <FormControlLabel
-//                         control={<Checkbox
-//                             slotProps={{
-//                                 input: {
-//                                     ref: isGifRef,
-//                                 },
-//                             }}
-//                             defaultChecked
-//                         />}
-//                         label="гифки"
-//                     />
-//                 </FormGroup>
-//             </Box>
-
-
-//             <Box sx={{
-//                 columnCount: { xs: 1, sm: 2, md: 3, lg: 4 },
-//                 columnGap: '1em',
-//                 mt: '2em'
-//             }}>
-//                 {data.map((elem) => <ListItem source={elem.source} data={elem.data} key={nanoid()} />)}
-
-//                 <div ref={sentryRef} />
-//             </Box>
-//         </Box >
-//     );
-// }
-
-// export default DashboardPage;
-
-import { Box, TextField, InputAdornment, Button, FormGroup, Checkbox, FormControlLabel, Typography } from "@mui/material";
+import { Box, TextField, InputAdornment, Button, FormGroup, Checkbox, FormControlLabel, Typography, Skeleton } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import { nanoid } from "nanoid";
 import ListItem from "../../components/ListItem";
@@ -259,27 +11,25 @@ import { Bounce, toast } from "react-toastify";
 import { combineAndShuffleArrays } from "../../util/dashboard";
 import { setSimpleField } from "../SignUpPage/AuthSlice";
 import useInfiniteScroll from 'react-infinite-scroll-hook';
+import Masonry from 'react-masonry-css'
 
 const DashboardPage = () => {
     const dispatch = useDispatch();
     const { page, query, hasMore, tenorNext, data } = useSelector((state) => state.dashboard);
     const { loading } = useSelector(state => state.auth);
 
-    // Мемоизированные refs
     const queryRef = useRef(null);
     const isImgRef = useRef(null);
     const isVideoRef = useRef(null);
     const isGifRef = useRef(null);
     const lastSearchTimeRef = useRef(0);
 
-    // Получение актуальных значений фильтров из refs
     const getCurrentFilters = useCallback(() => ({
         isImg: isImgRef.current?.checked ?? true,
         isVideo: isVideoRef.current?.checked ?? true,
         isGif: isGifRef.current?.checked ?? true
     }), []);
 
-    // Мемоизированное уведомление
     const notify = useCallback(() => toast.error("Что-то пошло не так :(", {
         position: "bottom-left",
         autoClose: 3000,
@@ -291,7 +41,6 @@ const DashboardPage = () => {
         transition: Bounce,
     }), []);
 
-    // Мемоизированные функции для получения контента с актуальными фильтрами
     const getContentWithoutQuery = useCallback(async (filters) => {
         const { isImg, isGif } = filters;
 
@@ -373,8 +122,7 @@ const DashboardPage = () => {
         return combinedData;
     }, [page, dispatch, notify]);
 
-    // Оптимизированная функция загрузки с троттлингом
-    const loadMore = useCallback(throttle(async () => {
+    const unThrottledLoadMore = useCallback(async () => {
         const now = Date.now();
         if (now - lastSearchTimeRef.current < 500) return;
         lastSearchTimeRef.current = now;
@@ -386,9 +134,10 @@ const DashboardPage = () => {
         const currentFilters = getCurrentFilters();
 
         try {
-            const newData = query.length === 0
-                ? await getContentWithoutQuery(currentFilters)
-                : await getContentByQuery(currentFilters, query);
+            const newData =
+                query.length === 0
+                    ? await getContentWithoutQuery(currentFilters)
+                    : await getContentByQuery(currentFilters, query);
 
             dispatch(setData({ data: [...data, ...newData] }));
         } catch (error) {
@@ -396,10 +145,11 @@ const DashboardPage = () => {
         } finally {
             dispatch(setSimpleField({ field: 'loading', value: false }));
         }
-    }, 500, { leading: true, trailing: false }), [page, query, data, getContentWithoutQuery, getContentByQuery, dispatch, getCurrentFilters]);
+    }, [page, query, data, getContentWithoutQuery, getContentByQuery, dispatch, getCurrentFilters]);
 
-    // Оптимизированная функция нового поиска с дебаунсингом
-    const newFetch = useCallback(debounce(async () => {
+    const loadMore = throttle(unThrottledLoadMore, 500, { leading: true, trailing: false });
+
+    const unDebouncedNewFetch = useCallback(async () => {
         const now = Date.now();
         if (now - lastSearchTimeRef.current < 500) return;
         lastSearchTimeRef.current = now;
@@ -412,17 +162,20 @@ const DashboardPage = () => {
         const searchQuery = queryRef.current?.value?.replace(/[^a-zA-Z ]/g, "") || "";
         const currentFilters = getCurrentFilters();
 
-        dispatch(setQuery({
-            query: searchQuery,
-            isImg: currentFilters.isImg,
-            isVideo: currentFilters.isVideo,
-            isGif: currentFilters.isGif
-        }));
+        dispatch(
+            setQuery({
+                query: searchQuery,
+                isImg: currentFilters.isImg,
+                isVideo: currentFilters.isVideo,
+                isGif: currentFilters.isGif,
+            })
+        );
 
         try {
-            const newData = searchQuery.length === 0
-                ? await getContentWithoutQuery(currentFilters)
-                : await getContentByQuery(currentFilters, searchQuery);
+            const newData =
+                searchQuery.length === 0
+                    ? await getContentWithoutQuery(currentFilters)
+                    : await getContentByQuery(currentFilters, searchQuery);
 
             dispatch(setData({ data: newData }));
         } catch (error) {
@@ -430,14 +183,36 @@ const DashboardPage = () => {
         } finally {
             dispatch(setSimpleField({ field: 'loading', value: false }));
         }
-    }, 500), [getContentWithoutQuery, getContentByQuery, dispatch, getCurrentFilters]);
+    }, [getContentWithoutQuery, getContentByQuery, dispatch, getCurrentFilters]);
 
-    // Обработчик клика с немедленным вызовом
+    const newFetch = debounce(unDebouncedNewFetch, 500);
+
     const handleSearchClick = useCallback(() => {
         newFetch();
     }, [newFetch]);
 
-    // Мемоизированный список элементов
+    const memoizedSkeletons = useMemo(() =>
+        Array.from({ length: 4 }, () => {
+            const height = Math.floor(Math.random() * (500 - 200 + 1)) + 200;
+
+            return (
+                <Skeleton
+                    key={nanoid()}
+                    variant="rounded"
+                    animation="wave"
+                    sx={{
+                        width: '100%',
+                        height: height - height % 10,
+                        borderRadius: '1em',
+                        breakInside: 'avoid-column',
+                        mb: '1em',
+                        overflow: 'hidden'
+                    }}
+                />
+            );
+        }), []
+    );
+
     const memoizedListItems = useMemo(() =>
         data.map((elem) =>
             <ListItem source={elem.source} data={elem.data} key={nanoid()} />
@@ -448,6 +223,7 @@ const DashboardPage = () => {
         loading,
         hasNextPage: hasMore,
         onLoadMore: loadMore,
+        rootMargin: '0px 0px 2000px 0px',
     });
 
     return (
@@ -465,9 +241,6 @@ const DashboardPage = () => {
 
             <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '2em' }}>
                 <TextField
-                    InputProps={{
-                        style: { color: '#F2EBFB' },
-                    }}
                     size="small"
                     inputRef={queryRef}
                     type="text"
@@ -477,6 +250,7 @@ const DashboardPage = () => {
                     sx={{ minWidth: '50%' }}
                     slotProps={{
                         input: {
+                            style: { color: '#F2EBFB' },
                             startAdornment: (
                                 <InputAdornment position="start">
                                     <SearchIcon sx={{ color: '#F2EBFB' }} />
@@ -484,7 +258,7 @@ const DashboardPage = () => {
                             ),
                         },
                     }}
-                    onKeyPress={(e) => {
+                    onKeyDown={(e) => {
                         if (e.key === 'Enter') {
                             handleSearchClick();
                         }
@@ -530,14 +304,20 @@ const DashboardPage = () => {
                 </FormGroup>
             </Box>
 
-            <Box sx={{
-                columnCount: { xs: 1, sm: 2, md: 3, lg: 4 },
-                columnGap: '1em',
-                mt: '2em'
-            }}>
+            <Masonry
+                breakpointCols={{
+                    default: 4,
+                    1100: 3,
+                    700: 2,
+                    500: 1
+                }}
+                className="masonry-grid"
+                columnClassName="masonry-grid_column"
+            >
                 {memoizedListItems}
+                {(loading || hasMore) && memoizedSkeletons}
                 <div ref={sentryRef} />
-            </Box>
+            </Masonry>
         </Box>
     );
 }
