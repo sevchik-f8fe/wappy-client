0e636d83750f9861d541a6f6b25a8bd7169ac1b4
sevchik-f8fe <selddevelopment1111@gmail.com>
2025-11-13 14:44:12 +0300

    f

--- src/util/axiosConfig.js
+++ src/util/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_SERVER_URL,
+    baseURL: 'http://127.0.0.1:3000',
     withCredentials: true,
     timeout: 5000,
 });
