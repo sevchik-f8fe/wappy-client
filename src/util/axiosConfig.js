2425c67803f16224a0a51beb2253125e439b95ec
sevchik-f8fe <selddevelopment1111@gmail.com>
2026-06-16 20:59:22 +0300

    prod

--- src/util/axiosConfig.js
+++ src/util/axiosConfig.js
@@ -9,7 +9,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://161.104.16.134:3000',
+    // baseURL: 'http://127.0.0.1:3000',
     withCredentials: true,
     timeout: 5000,
 });
